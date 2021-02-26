f04da750acdfe90d5419b6a604827187c9692990
shen-sat <shenthuran.satkunarasa@fundingcircle.com>
2021-02-26 12:10:05 +0000

    Add test to copy file

--- lib/mutator.rb
+++ lib/mutator.rb
@@ -1,2 +1,10 @@
 class Mutator
+  def create_copy(filename)
+    in_file = File.read(filename)
+
+    in_filename_partial = filename[/.+[^_spec.rb]/]
+
+    out_filename = "#{in_filename_partial}_copy_spec.rb"
+    out_file = File.write(out_filename, in_file)
+  end
 end
--- spec/mutator_spec.rb
+++ spec/mutator_spec.rb
@@ -1,4 +1,21 @@
 require_relative '../lib/mutator'
 
 RSpec.describe Mutator do
+  
+  it 'copies file' do
+    filename = 'fixture/hello_spec.rb'
+    file = File.read(filename)
+
+    mutator = Mutator.new
+    mutator.create_copy(filename)
+
+    copy_filename = 'fixture/hello_copy_spec.rb'
+    copy_file = File.read(copy_filename)
+
+    expect(file).to eq(copy_file)
+
+    File.delete(copy_filename)
+  end
+  
+  
 end
