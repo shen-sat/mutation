1f9560bb0c8b48943ac0a4409308fb226ef37d16
shen-sat <shenthuran.satkunarasa@fundingcircle.com>
2021-02-27 11:22:33 +0000

    Add run method, works for a swapped, passing spec

--- lib/mutator.rb
+++ lib/mutator.rb
@@ -1,20 +1,34 @@
 class Mutator
-  def create_copy(filename)
-    in_file = File.read(filename)
-
-    in_filename_partial = filename[/.+[^_spec.rb]/]
-
-    out_filename = "#{in_filename_partial}_copy_spec.rb"
-    out_file = File.write(out_filename, in_file)
-  end
-
   def run_spec(filename)
     system("rspec #{filename}", :out => File::NULL)
 
     $?
   end
 
-  def swap(content, current_word, new_word)
-    content.gsub(current_word, new_word)
+  def swap(content, current_text, new_text)
+    content.gsub!(current_text, new_text)
+  end
+
+  def check_for_line_match(line, match_text)
+    return true if line.match(/#{match_text}/)
+
+    false
+  end
+
+  def write_lines_to_file(filename, lines)
+    content = lines.join()
+    File.write(filename, content)
+  end
+
+  def run(filename, current_text, new_text)
+    lines = File.readlines(filename)
+
+    lines.each do |line|
+      next unless check_for_line_match(line, current_text)
+
+      swap(line, current_text, new_text)
+
+      write_lines_to_file(filename, lines)
+    end
   end
 end
--- spec/mutator_spec.rb
+++ spec/mutator_spec.rb
@@ -1,23 +1,6 @@
 require_relative '../lib/mutator'
 
 RSpec.describe Mutator do
-  
-  describe '#create_copy' do
-    it 'copies file' do
-      filename = 'fixture/hello_spec.rb'
-      file = File.read(filename)
-
-      mutator = Mutator.new
-      mutator.create_copy(filename)
-
-      copy_filename = 'fixture/hello_copy_spec.rb'
-      copy_file = File.read(copy_filename)
-
-      expect(file).to eq(copy_file)
-
-      File.delete(copy_filename)
-    end
-  end
 
   describe '#run_spec' do
     context 'for a passing test' do
@@ -45,13 +28,72 @@ RSpec.describe Mutator do
   describe '#swap' do
     it 'swaps strings' do
       content = "let(:foobar) { FactoryBot.create(:foobar) }\n"
-      current_word = 'FactoryBot.create'
-      new_word = 'FactoryBot.build'
+      current_text = 'FactoryBot.create'
+      new_text = 'FactoryBot.build'
       mutator = Mutator.new
 
-      new_content = mutator.swap(content, current_word, new_word)
+      new_content = mutator.swap(content, current_text, new_text)
 
       expect(new_content).to eq("let(:foobar) { FactoryBot.build(:foobar) }\n")
+    end
+  end
+
+  describe '#check_for_line_match' do
+    match_text = 'FactoryBot.create'
+    context 'if line is a match' do
+      line = "let(:foobar) { FactoryBot.create(:foobar) }\n"
+      it 'returns true' do
+        mutator = Mutator.new
+
+        expect(mutator.check_for_line_match(line, match_text)).to eq(true)
+      end
+    end
+    context 'if line is not a match' do
+      line = "let(:foobar) { FactoryBot.build(:foobar) }\n"
+      it 'returns false' do
+        mutator = Mutator.new
+
+        expect(mutator.check_for_line_match(line, match_text)).to eq(false)
+      end
+    end
+  end
+
+  describe '#write_lines_to_file' do
+    it 'writes contents to file' do
+      lines = ['hello', 'world']
+      filename = 'fixture/hello_world_spec.rb'
+
+      FileUtils.touch filename
+
+      mutator = Mutator.new
+      mutator.write_lines_to_file(filename, lines)
+
+      expect(File.read(filename)).to eq('helloworld')
+
+      File.delete(filename)
+    end
+  end
+
+  describe '#run' do
+    context 'when swapped text passes test' do
+      it 'swaps text' do
+        #setup
+        content_for_spec = "RSpec.describe 'FactoryBot.create' do\n  \n  it 'passes' do\n    expect(1).to eq(1)\n  end\nend\n"
+
+        filename = 'fixture/foobar_spec.rb'
+
+        File.write(filename, content_for_spec)
+        #test setup passes
+        expect(system("rspec #{filename}", :out => File::NULL)).to eq(true)
+
+        #test
+        mutator = Mutator.new
+        mutator.run(filename, 'FactoryBot.create', 'FactoryBot.build')
+
+        expect(File.read(filename)).to eq("RSpec.describe 'FactoryBot.build' do\n  \n  it 'passes' do\n    expect(1).to eq(1)\n  end\nend\n")
+
+        File.delete(filename) 
+      end
     end
   end
 end
