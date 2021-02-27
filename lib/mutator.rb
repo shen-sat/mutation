ef1e3820a5ef4e90d2e45ccefd07c90cb2321b94
Shen Sat <shensat@shens-mbp.home>
2021-02-27 16:03:35 +0000

    Refactor

--- lib/mutator.rb
+++ lib/mutator.rb
@@ -1,15 +1,37 @@
 class Mutator
-  def run_spec(filename)
+  def run(filename, current_text, new_text)
+    lines = File.readlines(filename)
+
+    lines.each do |line|
+      next unless line_match?(line, current_text)
+
+      swap(line, current_text, new_text)
+
+      write_lines_to_file(filename, lines)
+
+      if !spec_passes?(filename)
+        swap(line, new_text, current_text)
+        write_lines_to_file(filename, lines)        
+      end
+    end
+  end
+
+  private
+
+  def spec_passes?(filename)
     system("rspec #{filename}", :out => File::NULL)
 
-    $?
+    result = $?
+
+    return true if result == 0
+    false
   end
 
-  def swap(content, current_text, new_text)
-    content.gsub!(current_text, new_text)
+  def swap(line, current_text, new_text)
+    line.gsub!(current_text, new_text)
   end
 
-  def check_for_line_match(line, match_text)
+  def line_match?(line, match_text)
     return true if line.match(/#{match_text}/)
 
     false
@@ -18,24 +40,5 @@ class Mutator
   def write_lines_to_file(filename, lines)
     content = lines.join()
     File.write(filename, content)
-  end
-
-  def run(filename, current_text, new_text)
-    lines = File.readlines(filename)
-
-    lines.each do |line|
-      next unless check_for_line_match(line, current_text)
-
-      swap(line, current_text, new_text)
-
-      write_lines_to_file(filename, lines)
-
-      result = run_spec(filename)
-
-      if result.exitstatus != 0
-        swap(line, new_text, current_text)
-        write_lines_to_file(filename, lines)        
-      end
-    end
   end
 end
--- spec/mutator_spec.rb
+++ spec/mutator_spec.rb
@@ -1,93 +1,19 @@
 require_relative '../lib/mutator'
 
 RSpec.describe Mutator do
-
-  describe '#run_spec' do
-    context 'for a passing test' do
-      it 'returns exit code 0' do
-        filename = 'fixture/passing_spec.rb'
-        mutator = Mutator.new
-
-        result = mutator.run_spec(filename);
-
-        expect(result.exitstatus).to eq(0)
-      end
-    end
-    context 'for a failing test' do
-      it 'returns exit code 0' do
-        filename = 'fixture/failing_spec.rb'
-        mutator = Mutator.new
-
-        result = mutator.run_spec(filename)
-
-        expect(result.exitstatus).not_to eq(0)
-      end
-    end
-  end
-
-  describe '#swap' do
-    it 'swaps strings' do
-      content = "let(:foobar) { FactoryBot.create(:foobar) }\n"
-      current_text = 'FactoryBot.create'
-      new_text = 'FactoryBot.build'
-      mutator = Mutator.new
-
-      new_content = mutator.swap(content, current_text, new_text)
-
-      expect(new_content).to eq("let(:foobar) { FactoryBot.build(:foobar) }\n")
-    end
-  end
-
-  describe '#check_for_line_match' do
-    match_text = 'FactoryBot.create'
-    context 'if line is a match' do
-      line = "let(:foobar) { FactoryBot.create(:foobar) }\n"
-      it 'returns true' do
-        mutator = Mutator.new
-
-        expect(mutator.check_for_line_match(line, match_text)).to eq(true)
-      end
-    end
-    context 'if line is not a match' do
-      line = "let(:foobar) { FactoryBot.build(:foobar) }\n"
-      it 'returns false' do
-        mutator = Mutator.new
-
-        expect(mutator.check_for_line_match(line, match_text)).to eq(false)
-      end
-    end
-  end
-
-  describe '#write_lines_to_file' do
-    it 'writes contents to file' do
-      lines = ['hello', 'world']
-      filename = 'fixture/hello_world_spec.rb'
-
-      FileUtils.touch filename
-
-      mutator = Mutator.new
-      mutator.write_lines_to_file(filename, lines)
-
-      expect(File.read(filename)).to eq('helloworld')
-
-      File.delete(filename)
-    end
-  end
+ let(:mutator) { Mutator.new }
 
   describe '#run' do
+    let(:filename) { 'tmp/example_spec.rb' }
+
+    before { File.write(filename, content_for_spec) }
+
     context 'when swapped text passes test' do
+      let(:content_for_spec) { "RSpec.describe 'FactoryBot.create' do\n  \n  it 'passes' do\n    expect(1).to eq(1)\n  end\nend\n" }
+      
       it 'swaps text' do
-        #setup
-        content_for_spec = "RSpec.describe 'FactoryBot.create' do\n  \n  it 'passes' do\n    expect(1).to eq(1)\n  end\nend\n"
-
-        filename = 'fixture/foobar_spec.rb'
-
-        File.write(filename, content_for_spec)
-        #test setup passes
         expect(system("rspec #{filename}", :out => File::NULL)).to eq(true)
 
-        #test
-        mutator = Mutator.new
         mutator.run(filename, 'FactoryBot.create', 'FactoryBot.build')
 
         expect(File.read(filename)).to eq("RSpec.describe 'FactoryBot.build' do\n  \n  it 'passes' do\n    expect(1).to eq(1)\n  end\nend\n")
@@ -96,18 +22,11 @@ RSpec.describe Mutator do
       end
     end
     context 'when swapped text does not pass test' do
+      let(:content_for_spec) { "RSpec.describe 'FactoryBot.create' do\n  \n  it 'passes' do\n    expect(1).to eq(2)\n  end\nend\n" }
+      
       it 'does not swap text' do
-        #setup
-        content_for_spec = "RSpec.describe 'FactoryBot.create' do\n  \n  it 'passes' do\n    expect(1).to eq(2)\n  end\nend\n"
-
-        filename = 'fixture/foobar_spec.rb'
-
-        File.write(filename, content_for_spec)
-        #test setup passes
         expect(system("rspec #{filename}", :out => File::NULL)).to eq(false)
 
-        #test
-        mutator = Mutator.new
         mutator.run(filename, 'FactoryBot.create', 'FactoryBot.build')
 
         expect(File.read(filename)).to eq("RSpec.describe 'FactoryBot.create' do\n  \n  it 'passes' do\n    expect(1).to eq(2)\n  end\nend\n")
